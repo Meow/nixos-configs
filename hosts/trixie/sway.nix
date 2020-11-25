840c6ac09e54d5a0762dc59c266637ff73c313b8
Luna D <cod7777@yandex.ru>
2020-11-25 15:43:23 +0100

    keyboard

--- hosts/trixie/sway.nix
+++ hosts/trixie/sway.nix
@@ -3,7 +3,7 @@
   environment = {
     etc = {
       "sway/machine_specific".text = ''
-        set $keyboard_id "4152:5639:SteelSeries_SteelSeries_Apex_M500_Gaming_Keyboard"
+        set $keyboard_id "6940:6985:Corsair_CORSAIR_K70_RGB_MK.2_Mechanical_Gaming_Keyboard"
         set $mouse_id "4152:4961:La-VIEW_Technology_SteelSeries_Sensei"
       '';
     };
