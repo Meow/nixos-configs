1b6cd24f0e4d8703a07c58189c87176e2d3ba983
Luna D <cod7777@yandex.ru>
2020-12-02 16:56:30 +0100

    stuff

--- hosts/rainy/sway.nix
+++ hosts/rainy/sway.nix
@@ -6,9 +6,9 @@
         set $keyboard_id "6940:6985:Corsair_CORSAIR_K70_RGB_MK.2_Mechanical_Gaming_Keyboard"
         set $mouse_id "6899:1:Kingsis_Peripherals_ZOWIE_Gaming_mouse"
 
-        output HDMI-A-1 resolution 1920x1080 position 0,0 scale 1
-        output DP-2 resolution 1920x1080@240Hz position 1920,0 scale 1
-        output DP-3 resolution 3840x2160 position 3840,0 scale 2
+        output HDMI-A-1 mode 1920x1080 position 0 0 scale 1
+        output DP-2 mode 1920x1080@240Hz position 1920 0 scale 1 adaptive_sync off
+        output DP-3 mode 3840x2160 position 3840 0 scale 2
         
         input $mouse_id pointer_accel -0.5
       '';
